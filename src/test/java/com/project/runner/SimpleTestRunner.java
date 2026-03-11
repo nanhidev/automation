7a63ab3826a0e12a7c6dde5f78d0bc72dcf94df9
nanhidev <nanhi.kumari@walkingtree.tech>
2026-03-11 12:30:30 +0530

    Auto-generated 7 file(s) - 2026-03-11_12-30-29

--- src/test/java/com/project/runner/SimpleTestRunner.java
+++ src/test/java/com/project/runner/SimpleTestRunner.java
@@ -17,6 +17,11 @@ package com.project.runner;
             runCucumberFeature("src/test/resources/com/automation/features/Sheet1.feature");
         }
     
+    @Test(priority = 1)
+        public void runLoginscreen() {
+            runCucumberFeature("src/test/resources/com/automation/features/LoginScreen.feature");
+        }
+    
             
     private void runCucumberFeature(String featurePath) {
             String featureName = new File(featurePath).getName().replace(".feature", "");
